e60fa329c1fa14e873cdfc8e795755bc83224d1a
Bumindu Yasith <bumiduyasith@gmail.com>
2023-04-15 19:52:52 +0530

    install basic dependency and create server.js file as the starting point

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+var cookieParser = require('cookie-parser')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cookieParser())
+
+app.get('/', (req,res)=>{
+
+    console.log('Cookies: ', req.cookies.token)
+   
+    return res.status(200).json({
+        "done":true
+    })
+});
+
+app.get('/login', (req,res)=>{
+
+    res.cookie("token", "123", {
+
+        httpOnly:true,
+        maxAge:100000
+    });
+
+    return res.json({
+        "islogged": true
+    })
+});
+
+app.get('/logout', (req,res)=>{
+
+   res.clearCookie("token");
+   res.json({
+    "islogged": false
+   })
+});
+
+
+
+app.listen(port, ()=>{
+
+    console.log(`server running on port ${port}`);
+});

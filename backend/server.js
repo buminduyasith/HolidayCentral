e6feb53685477e709b5b3786c25f534f72b990d2
Bumindu Yasith <bumiduyasith@gmail.com>
2023-04-26 21:32:25 +0530

    create authcontroller

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
+const authController = require('./controllers/authController.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cookieParser())
+app.use(express.json());
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
 
     console.log('Cookies: ', req.cookies.token)
-   
+
     return res.status(200).json({
-        "done":true
+        "done": true
     })
 });
 
-app.get('/login', (req,res)=>{
+
+
+app.get('/login', (req, res) => {
 
     res.cookie("token", "123", {
 
-        httpOnly:true,
-        maxAge:100000
+        httpOnly: true,
+        maxAge: 100000
     });
 
     return res.json({
@@ -29,17 +33,28 @@ app.get('/login', (req,res)=>{
     })
 });
 
-app.get('/logout', (req,res)=>{
+app.get('/logout', (req, res) => {
 
-   res.clearCookie("token");
-   res.json({
-    "islogged": false
-   })
+    res.clearCookie("token");
+    res.json({
+        "islogged": false
+    })
+});
+
+app.post('/signup', (req, res) => {
+
+    console.log(req.body.username)
+    res.json({
+        "islogged": false
+    })
 });
 
 
+app.use('/auth', authController)
 
-app.listen(port, ()=>{
+
+
+app.listen(port, () => {
 
     console.log(`server running on port ${port}`);
 });

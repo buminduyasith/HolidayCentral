d29ea2fee5686b332c32e1c8ec54fd4d9b257ce9
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-04-26 21:32:02 +0530

    fix(Server): Route Fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 5000;
 
-const flights = require('./routs/flights.js');
+const flights = require('./routes/flights.js');
 
 app.use(cookieParser())
 

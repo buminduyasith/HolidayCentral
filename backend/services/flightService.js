7ae01eb36683b59969939a7ed72ba1657ce5fe5a
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-04-27 22:40:58 +0530

    feat(Flights BE) : Flight Get Functions

--- backend/services/flightService.js
+++ backend/services/flightService.js
@@ -1,3 +1,15 @@
 const flightModel =  require('../models/FlightModel')
 const userRoles = require("../enums/userRoles");
 const { v4: uuidv4 } = require('uuid');
+
+async function getAllFlights(params) {
+    const allFlightData = await flightModel.find({});
+    return allFlightData
+}
+
+async function getSearchAllFlights(params) {
+    const filter = {}
+    const allFlightData = await flightModel.find(filter);
+    return allFlightData
+}
+

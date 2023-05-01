c4f52374e6b11af06caf6ff5425d8f515009c6ce
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-01 12:57:34 +0530

    insert flight details

--- backend/controllers/backOfficeController.js
+++ backend/controllers/backOfficeController.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
+const path = require("path");
 
 const userRoles = require("../enums/userRoles");
 const { InsertFlightDetails } = require("../services/backofficeuserService");
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); // Change the upload directory as needed
+    },
+    filename: (req, file, cb) => {
+        const fileExt = path.extname(file.originalname);
+        const fileName = file.originalname.replace(fileExt, "").toLowerCase().split(" ").join("-") + "-" + Date.now();
+        cb(null, fileName + fileExt);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.includes("csv")) {
+            cb(null, true);
+        } else {
+           // cb(new Error("Only CSV files are allowed."));
+            const error = new Error('Only CSV files are allowed.');
+            error.status = 400;
+            cb(error);
+        }
+    },
+});
 
 router.get("/", (req, res) => {
     res.send("admin bo");
 });
 
-router.post("/flights", upload.single(""), async (req, res) => {
-    const csvPath = req.file;
-    console.log("csvPath", csvPath);
-    res.send(csvPath);
+router.post("/flights", upload.single("file"), async (req, res, next) => {
+    try {
+        //const csvPath = req.file.path
+        const filePath = path.join(process.cwd(), req.file.path);
+        console.log("csvPath", filePath);
+        await InsertFlightDetails(filePath);
+        res.send(filePath);
+    } catch (error) {
+        console.log("flights details save", error)
+        res.status(400)
+        next(error);
+    }
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const authController = require("./controllers/authController");
 const backOfficeController = require("./controllers/backOfficeController");
 const { verifyTokenAndSetUser, isLoggedIn } = require("./middlewares/authenticationMiddleware");
@@ -10,6 +11,8 @@ const userRoles = require('./enums/userRoles')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());

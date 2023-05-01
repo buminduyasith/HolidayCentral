aa5178df80b78913101ca41b46d94d2b9e4c2b6f
cpdevmd7 <cpdev.md7@gmail.com>
2023-05-01 19:44:22 +0530

    create hotel model

---
+++ backend/models/HotelModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose")
+const  {schema} = mongoose;
+
+const HotelSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    photos:{
+        type: [String],
+    },
+    desc:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        min:0,
+        max:5
+    },
+    rooms:{
+        type: [String],
+    },
+    Price:{
+        type: Number,
+        required: true
+    },
+    checkInDate:{
+        type: String,
+        required: true
+    },
+    checkOutDate:{
+        type: String,
+        required: true
+    },
+    facilities:{
+        type: [String],
+    },
+    featured:{
+        type: Boolean,
+        default: false,
+    },
+
+});
+module.exports=mongoose.model("Hotels", HotelSchema); // Hotel => DB collection name

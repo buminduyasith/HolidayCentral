0d67bd04f80c9240b857996459619a5afd7d713b
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-02 19:36:18 +0530

    implement upload packages related data csv

---
+++ frontend/src/pages/backoffice/holidaypackages/add/index.jsx
@@ -0,0 +1,42 @@
+import Layout from "@/components/layout/layout";
+import React, { useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import axios from "axios";
+import BONavBar from "@/components/navbar/BONavBar";
+
+export default function index() {
+    const { data: session } = useSession();
+    const [file, setFile] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/v1/backoffice/product/packages", formData);
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+    return (
+        <>
+            <BONavBar />
+            <div className="container">
+                <h2 className="mt-3">Upload Holiday Packages</h2>
+                <form onSubmit={handleSubmit} className="mt-3">
+                    <label htmlFor="formFileLg" className="form-label mb-3">
+                    Please choose the CSV file that you would like to upload to the database
+                    </label>
+                    <input className="form-control form-control-lg" id="formFileLg" name="file" type="file" onChange={handleFileChange} />
+                    <button className="btn btn-primary mt-3" type="submit">Upload</button>
+                </form>
+            </div>
+        </>
+    );
+}

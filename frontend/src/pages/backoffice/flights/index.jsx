39fae8d1aaa348a4bf1b143d43ef0840a3a5db39
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-03 11:26:46 +0530

    use created axio hook to call protected endponits

--- frontend/src/pages/backoffice/flights/index.jsx
+++ frontend/src/pages/backoffice/flights/index.jsx
@@ -3,36 +3,48 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import BoFlightCard from "@/components/bocomponts/boFlightCard";
 import axios from "lib/axios";
 import BONavBar from "@/components/navbar/BONavBar";
+import useAxiosAuth from "lib/hooks/useAxiosAuth";
 
 export default function dashboard() {
     const { data: session } = useSession();
 
     const [flights, setFlights] = useState([]);
-    const [isloading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(true);
+
+    const axiosAuth = useAxiosAuth();
 
     useEffect(() => {
-        axios
-        .get(`api/v1/backoffice/product/flights`)
-        .then((res) => {
-            console.log(res.data)
-            setFlights(res.data)
-            setLoading(false)
-        })
-        .catch((error) => {
-            console.error(error);
-            setLoading(false)
-        });
-        setFlights(flights);
+        const myTimeout = setTimeout(() => {
+            axiosAuth
+                .get(`api/v1/backoffice/product/flights`)
+                .then((res) => {
+                    console.log(res.data);
+                    setFlights(res.data);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setLoading(false);
+                });
+            setFlights(flights);
+        }, 5000);
     }, []);
 
     return (
         <>
-        <BONavBar />
+            <BONavBar />
             <div className="container">
                 <h1 className="my-5">All Flights</h1>
-                {flights?.map((flight) => (
-                    <BoFlightCard key={flight._id} flight={flight} />
-                ))}
+
+                {isLoading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                        {flights.map((flight) => (
+                            <BoFlightCard key={flight._id} flight={flight} />
+                        ))}
+                    </>
+                )}
             </div>
         </>
     );

505702691a6ac1ec6adf207e2d280042438d8e67
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-03 11:58:28 +0530

    create bo sign up validation schema

---
+++ backend/schemas/boSignupSchema.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+const boSignupSchema = Joi.object({
+    firstName: Joi.string()
+        .required(),
+
+    lastName: Joi.string()
+        .required(),    
+    
+    phoneNumber: Joi.string()
+        .min(10)
+        .max(10),
+
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com'] } })
+})
+
+module.exports = boSignupSchema;

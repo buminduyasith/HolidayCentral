d2771adb748ddf434c2cf4a2d9e3136b23e118f4
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-03 14:36:04 +0530

    move insertFlightdetails function into backofficeflightservice

--- backend/services/backofficeuserService.js
+++ backend/services/backofficeuserService.js
@@ -1,8 +1,4 @@
-const fs = require("fs");
-const { parse } = require("csv-parse");
-const path = require("path");
 const boUserModel = require("../models/BackOfficeUserModel");
-const flightModel = require("../models/FlightModel");
 const { SendBOUserAccountCreationEmail } = require("../services/emailService");
 
 async function CreateBackofficeUser(boUser, identiyUser) {
@@ -14,27 +10,6 @@ async function CreateBackofficeUser(boUser, identiyUser) {
     return newBoUser;
 }
 
-async function InsertFlightDetails(csvPath) {
-    console.log("csvPath", csvPath);
-    const flightRecords = await new Promise((resolve, reject) => {
-        const parser = parse({ columns: true }, (err, records) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(records);
-            }
-        });
-        fs.createReadStream(csvPath).pipe(parser);
-    });
-
-    const flightModels = flightRecords.map((flightRecord) => new flightModel(flightRecord));
-    const results = await flightModel.insertMany(flightModels);
-
-    console.log("flight detail results", results);
-    return results;
-}
-
 module.exports = {
     CreateBackofficeUser,
-    InsertFlightDetails,
 };

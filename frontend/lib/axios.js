6387e89bbd87469f1885edc9e085ab39d1b3404a
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-03 11:25:36 +0530

    create another axios instance for protected  endpoints

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -4,3 +4,8 @@ export default axios.create({
     baseURL:"http://localhost:5000/",
     headers: {"Content-Type" : "application/json"}
 })
+
+export const axiosAuth = axios.create({
+    baseURL:"http://localhost:5000/",
+    headers: {"Content-Type" : "application/json"}
+})

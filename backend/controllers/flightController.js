f45c76764f6fa09c1cffe96803ff8e1845c20450
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-05-03 19:05:08 +0530

    feat(BE) : Added Logger Middlerware

--- backend/controllers/flightController.js
+++ backend/controllers/flightController.js
@@ -2,16 +2,24 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const { getAllFlights, getSearchAllFlights, createFlightCheckoutRecord } = require("../services/flightService");
+const fs = require("fs");
+const path = require("path");
+const { logger } = require("../middlewares/loggerMiddleware");
+
+const logFilePath = path.join(__dirname, "../logs/api.log");
+const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
 
 router.use(cors());
+
+router.use(logger);
 
 router.get("/allflights", async (req, res, next) => {
     try {
         const response = await getAllFlights();
         console.log("res", response);
-        res.send(response).status(200);
+        res.status(200).json(response);
     } catch (error) {
-        res.status(500);
+        res.status(500).json({ error: "Internal Server Error" });
         next(error);
     }
 });
@@ -20,9 +28,9 @@ router.get("/searchflights", async (req, res, next) => {
     try {
         const response = await getSearchAllFlights(req)
         console.log("res", response);
-        res.send(response).status(200);
+        res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(400).json({ error: "Invalid input" });
         next(error);
     }
 });
@@ -31,9 +39,9 @@ router.post("/checkout", async (req, res, next) => {
     try {
         const response = await createFlightCheckoutRecord(req)
         console.log("res", response);
-        res.sendStatus(201);
+        res.status(201).send("Flight checkout record created successfully");
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: "Internal Server Error" });
         next(error);
     }
 });

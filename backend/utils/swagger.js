f2f4cf7541b57584e0f6be01839a3a82215264ff
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-04 20:00:00 +0530

    add swagger config

---
+++ backend/utils/swagger.js
@@ -0,0 +1,30 @@
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Hello World",
+            version: "1.0.0",
+        },
+        components:{
+            securitySchemas: {
+                bearerAuth : {
+                    type: "http",
+                    schema:"bearer",
+                    bearerFormat:"JWT"
+                }
+            }
+        },
+        security:[
+            {
+                bearerAuth:[]
+            }
+        ]
+    },
+    apis: ["./controllers/*.js"], // files containing annotations as above
+};
+
+
+
+module.exports = {
+    swaggerOptions,
+};

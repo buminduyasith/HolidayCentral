ba39c24f1aacf8a1d3d8a33a38379b7440786289
Bumindu Yasith <bumiduyasith@gmail.com>
2023-05-11 12:35:02 +0530

    redirection issue fixed

--- frontend/src/pages/traveladgent/dashboard.jsx
+++ frontend/src/pages/traveladgent/dashboard.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-
+import Layout from "@/components/layout/layout";
+import { Container } from "react-bootstrap";
 export default function dashboard() {
     const { data: session } = useSession();
     return (
-        <section className="text-center">
-            <div className="container">
-                <h1>travel adgent Dashoboard</h1>
-                {session?.user ? (
-                    <>
-                        Signed in as {session.user.email} <br />
-                        <button onClick={() => signOut()}>Sign out</button>
-                    </>
-                ) : (
-                    <>
-                        Not signed in <br />
-                        <button onClick={() => signIn()}>Sign in</button>
-                    </>
-                )}
-                <div></div>
-            </div>
-        </section>
+        <Layout>
+            <Container></Container>
+        </Layout>
     );
 }
